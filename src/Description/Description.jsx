23652163e6218cd3c8c2289797d6d6e6e682a159
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-03-05 19:54:23 +0200

    add description

---
+++ src/Description/Description.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "../Description.module.css";
+
+const Description = () => {
+  return (
+    <div className={styles.descriptionContainer}>
+      <p className={styles.descriptionText}>
+        Please leave your feedback about our service by selecting one of the options below.
+      </p>
+    </div>
+  );
+};
+
+export default Description;
