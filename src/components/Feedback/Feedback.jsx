53e33a16eae7f0cd65e6af913a886cf709967572
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-03-20 19:59:58 +0200

    delete h2 in feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,7 +4,7 @@ import styles from "./Feedback.module.css";
 const Feedback = ({ feedback, totalFeedback, positiveFeedback }) => {
   return (
     <div className={styles.feedbackContainer}>
-      <h2 className={styles.feedbackTitle}>Feedback Statistics</h2>
+      {/* <h2 className={styles.feedbackTitle}>Feedback Statistics</h2> */}
       <p className={styles.feedbackItem}>Good: {feedback.good}</p>
       <p className={styles.feedbackItem}>Neutral: {feedback.neutral}</p>
       <p className={styles.feedbackItem}>Bad: {feedback.bad}</p>
